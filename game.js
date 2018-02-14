0331b9129a9607e31058ff47ed9820848b677575
Malrook <35943838+Malrook@users.noreply.github.com>
2018-02-13 21:07:46 -0500

    more coins and platforms
    new updated game here

--- game.js
+++ game.js
@@ -1,33 +1,54 @@
 // define variables
-var game;
-var player;
-var platforms;
-var badges;
-var items;
-var cursors;
-var jumpButton;
-var text;
-var winningMessage;
-var won = false;
-var currentScore = 0;
-var winningScore = 100;
+let game,
+    player,
+    platforms,
+    badges,
+    items,
+    cursors,
+    jumpButton,
+    text,
+    winningMessage,
+    won = false,
+    currentScore = 0,
+    winningScore = 100;
 
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin');
+  createItem(375, 400, 'coin');
+  createItem(575, 500, 'coin');
+  createItem(225, 500, 'coin');
+  createItem(100, 250, 'coin');
+  createItem(575, 150, 'coin');
+  createItem(525, 300, 'coin');
+  createItem(650, 250, 'coin');
+  createItem(225, 200, 'coin');
+  createItem(375, 100, 'poison');
+  createItem(370,500,'poison');
+  createItem(100, 375, 'poison');
+  createItem(125, 50, 'star');
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(450, 150, 'platform');
+  platforms.create(450, 550, 'platform');
+  platforms.create(100, 550, 'platform');
+  platforms.create(300, 450, 'platform');
+  platforms.create(250, 150, 'platform');
+  platforms.create(50, 300, 'platform');
+  platforms.create(150, 250, 'platform');
+  platforms.create(650, 300, 'platform');
+  platforms.create(550, 200, 'platform2');
+  platforms.create(300, 450, 'platform2');
+  platforms.create(400, 350, 'platform2');
+  platforms.create(100, 100, 'platform2');
   platforms.setAll('body.immovable', true);
 }
 
 // create a single animated item and add to screen
 function createItem(left, top, image) {
-  var item = items.create(left, top, image);
+  let item = items.create(left, top, image);
   item.animations.add('spin');
   item.animations.play('spin', 10, true);
 }
@@ -35,7 +56,7 @@ function createItem(left, top, image) {
 // create the winning badge and add to screen
 function createBadge() {
   badges = game.add.physicsGroup();
-  var badge = badges.create(750, 400, 'badge');
+  let badge = badges.create(750, 400, 'badge');
   badge.animations.add('spin');
   badge.animations.play('spin', 10, true);
 }
@@ -43,8 +64,14 @@ function createBadge() {
 // when the player collects an item on the screen
 function itemHandler(player, item) {
   item.kill();
-  currentScore = currentScore + 10;
-  if (currentScore === winningScore) {
+  if (item.key === 'coin') {
+     currentScore = currentScore + 10;
+  } else if (item.key === 'poison') {
+     currentScore = currentScore - 25;
+  } else if (item.key === 'star') {
+     currentScore = currentScore + 25;
+  }
+  if (currentScore >= winningScore) {
       createBadge();
   }
 }
@@ -65,11 +92,14 @@ window.onload = function () {
     
     //Load images
     game.load.image('platform', 'platform_1.png');
+    game.load.image('platform2', 'platform_2.png');
     
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
     game.load.spritesheet('coin', 'coin.png', 36, 44);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('star', 'star.png', 32, 32);
   }
 
   // initial game set up
@@ -85,7 +115,7 @@ window.onload = function () {
     addPlatforms();
 
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Arial", fill: "white" });
     winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white" });
     winningMessage.anchor.setTo(0.5, 1);
@@ -114,12 +144,13 @@ window.onload = function () {
     // player doesn't move
     else {
       player.animations.stop();
+      player.frame = 1;
     }
     
     if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
       player.body.velocity.y = -400;
     }
-    // when the player winw the game
+    // when the player wins the game
     if (won) {
       winningMessage.text = "YOU WIN!!!";
     }
